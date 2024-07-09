8f8d1f62f9edfae04a4fa02e442ac54ae01a672f
Arjun Ashok <125722020+for-trancer@users.noreply.github.com>
2024-07-09 23:54:37 +0530

    added comments

--- sql.php
+++ sql.php
@@ -1,8 +1,8 @@
 <?php
     $server = "localhost";
-    $user = "root";
-    $pass = "n0p@554ub27h";
-    $db = "nexus";
+    $user = "root"; // use your own username
+    $pass = ""; // replace with your password
+    $db = ""; // replace with your db name
 
     $conn = new mysqli($server,$user,$pass,$db);
     if($conn->connect_error)
